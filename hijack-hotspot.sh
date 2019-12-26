d178b6f959ee5ba6592038ec2bfffb6e662db9bb
Mateus Buogo <41753941+mbuogo@users.noreply.github.com>
2019-12-26 15:34:24 -0300

    Update hijack-hotspot.sh

--- hijack-hotspot.sh
+++ hijack-hotspot.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
+cont=0
+
 if [ "$1" == "" ] #testa o argumento
 then
         echo "Siga o exemplo abaixo para passagem de parametros. INTERFACE + SSID + RANGE IP"
-        echo "$0 [wlan1 | wlan0] SSID 192.168.1.0"
+        echo "$0 [wlan1 | wlan0] SSID 192.168.1."
 
 else
 
 	echo "#################################"
 	echo "######### HIJACK HOTSPOT ########"
 	echo "#################################"
+	
+	iwconfig $1 essid $2
+	
+	while [ $cont -lt 254 ]; do
+		ping $3$cont -c1
+	done;
 
-	echo "\n
+fi
+
+
+
+
+
+
+CONTADOR=0
+while [  $CONTADOR -lt 5 ]; do
+      echo "$CONTADOR";
+      let CONTADOR=CONTADOR+1; 
+done
